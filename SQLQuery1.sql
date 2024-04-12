6d0d24ccfdcebdae2e677e60ccf515e296299c03
Mihajlo Eskic <eskicmihajlo55@gmail.com>
2024-04-12 12:31:59 +0200

    functions and views

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -64,43 +64,122 @@ where STATUS_ROKA in ('redovni')
 
 
 
-6. Kreirati pogled koji prikazuje ime,prezime i kvalifikaciju izvršilaca i nazive projekata na kojima
-rade.
-CREATE VIEW Izvrsioci_projekti
-as
-SELECT ime,prezime,kvalif,naziv
-FROM IZVRSILAC,UCESCE,PROJEKAT
-WHERE IZVRSILAC.idIzvrsilac=UCESCE.idIzvrsilac AND
-UCESCE.idProjekat=PROJEKAT.idProjekat
-7. Kreirati pogled koji prikazuje izvršioce koji ne rade ni na jednom projektu.
-CREATE VIEW izvrsioci_bez_projekta
-as
-SELECT *
-FROM IZVRSILAC
-WHERE idIzvrsilac NOT IN (SELECT idIzvrsilac FROM UCESCE)
-8. Kreirati pogled koji prikazuje projekte na kojima ne radi nijedan izvršilac.
-CREATE VIEW projekti_bez_izvrsilaca
-as
-SELECT *
-FROM PROJEKAT
-WHERE idProjekat NOT IN (SELECT idProjekat FROM UCESCE)
+/*Kreirati pogled View_Prof_Predmet_>2 koji prikazuje imena i prezimena profesora koji
+predaju 2 ili više predmeta.*/
+create view pogledneki as
+select ime, prezime
+from profesor
+where ID_PROFESORA in (select ID_PROFESORA
+from predmet
+group by ID_PROFESORA
+having COUNT(*) >=2)
+
+/*Kreirati pogled koji prikazuje ime,prezime i kvalifikaciju izvršilaca i 
+nazive projekata na kojima
+rade.*/
+create view nekiPogled as 
+select ime , prezime, kvalif , naziv
+from IZVRSILAC, PROJEKAT , UCESCE
+where IZVRSILAC.idIzvrsilac = UCESCE.idIzvrsilac and
+PROJEKAT.idProjekat = UCESCE.idProjekat
+
+/*Kreirati pogled koji prikazuje izvršioce koji ne rade ni na jednom projektu*/
+select * 
+from IZVRSILAC
+where idIzvrsilac NOT IN (select idIzvrsilac from UCESCE)
+
+
+/*8. Kreirati pogled koji prikazuje projekte na kojima ne radi nijedan izvršilac*/
+select *
+from PROJEKAT
+where idProjekat not in ( select idProjekat from UCESCE )
 
 
 
-1. Kreirati funkciju fun_Staz koja vraća godine staza izvršioca čije se ime i prezime zadaju.
-● Prikazati koliko godina staža ima Rastko Simić koristeći funkciju fun_Radnik_Staz.
-● Kreirati upit koji prikazuje ime,prezime i godine staža svih izvršilaca primenom
-funkcije.
-Create function fun_SumStaz(@ime nchar(30), @prezime nchar(30)) returns int
+/*10. Kreirati pogled koji prikazuje naručioce i ukupan broj njihovih projekata.*/
+
+select NARUCILAC.idNarucilac, nazivFirme, COUNT(*) as BrojProjekata
+from NARUCILAC, PROJEKAT
+where NARUCILAC.idNarucilac = PROJEKAT.idNarucilac
+group by NARUCILAC.idNarucilac, nazivFirme
+
+
+
+
+/*Kreirati funkciju fun_profesor_predmet koja za prosleđeni id profesora vraća listu njegovih
+predmeta.*/
+
+create function fun_profesor_predmet (@idprofe int)
+returns table
+as return
+( select * 
+ from predmet
+ where ID_PROFESORA = @idprofe)
+
+ select * from fun_profesor_predmet (102)
+
+
+
+ /*Kreirati funkciju fun_prosek_predmet koja prihavata id_predmeta i vraća prosečnu
+ ocenu
+studenata na tom predmetu.*/
+create function fun_student_predmet_godina(@predmetID int) 
+returns float
 as begin
-declare @sumStaza int
-Select @sumStaza = SUM(datediff(yy,dat_zap,getdate())) FROM IZVRSILAC
-WHere ime=@ime and prezime=@prezime
-return @sumStaza end
-----------------------------------------------------------------------------------------------------------------
-SELECT dbo.fun_SumStaz('Rastko','Simić') as UkupanStaz
-—-------------------------------------------------------------------------------------------------------------
-select ime,prezime, dbo.fun_SumStaz(ime,prezime) as GodineStaza from IZVRSILAC
+declare @prosek float 
+select @prosek=avg(ocena)
+from zapisnik where
+ocena >5 and
+id_ispita in (select id_ispita
+from ispit
+where id_predmeta=@predmetID)
+return @prosek
+end
 
 
 
+/*Kreirati funkciju fun_student_predmet_godina koja prihvata indeks studenta i
+školsku godinu, a vraća spisak predmeta koje je student izabrao u toj školskoj godini.*/
+
+create function fun_student_predmet (@smer nvarchar(5) , @broj int , @godina nvarchar (4),
+@skolskagodina nvarchar(10))
+returns table
+as return
+(
+select predmet.ID_PREDMETA, NAZIV 
+from predmet , student, student_predmet
+where predmet.ID_PREDMETA = student_predmet.ID_PREDMETA and
+student.ID_STUDENTA = student_predmet.ID_STUDENTA
+and smer = @smer and BROJ = @broj and GODINA_UPISA = @godina and
+SKOLSKA_GODINA = @skolskagodina
+)
+
+select * from fun_student_predmet('NRT',15,'2019', '2019/20')
+
+
+
+/*Kreirati funkciju fun_Student_prosek koja za prosleđeni indeks studenta vraća
+njegovu prosečnu ocenu.*/
+
+create function fun_Student_prosek(@smer nvarchar(5), @broj int, @godina
+nvarchar(4))
+returns float
+as begin
+declare @prosek float
+select @prosek =AVG(OCENA)
+from zapisnik
+where ID_STUDENTA in
+(select ID_STUDENTA 
+from student
+where SMER = @smer and BROJ = @broj and GODINA_UPISA = @godina)
+return @prosek
+end
+
+select dbo.fun_Student_prosek ('IS',25,2019)
+
+
+
+
+/*Kreirati funkciju fun_narucilac_projekat koja za zadati id naručioca 
+vraća listu njegovih
+projekata.*/create function fun_narucilac_projekat (@idnarcioca int)returns tableas return(select idProjekat , nazivfrom PROJEKATwhere idNarucilac = @idnarcioca)select * from fun_narucilac_projekat ('1')
