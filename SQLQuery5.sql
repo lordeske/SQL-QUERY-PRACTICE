a16ff4298b23f2ca48add9017899ac090121f7fa
Mihajlo Eskic <eskicmihajlo55@gmail.com>
2024-05-22 22:08:15 +0200

    rollback

--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -1,5 +1,5 @@
-1.Kreirati uskladi?tenu proceduru sp_Premesti_OJ posredstvom koje se izvr?ilac prebacuje iz jedne u drugu
-organizacionu jedinicu. Proceduri se prosle?uju id organizacione jedinice u koju se prebacuje i id izvr?ioca koji se
+﻿1.Kreirati uskladitenu proceduru sp_Premesti_OJ posredstvom koje se izvrilac prebacuje iz jedne u drugu
+organizacionu jedinicu. Proceduri se prosle?uju id organizacione jedinice u koju se prebacuje i id izvrioca koji se
 prebacuje.
 
 create procedure sp_Premesti_OJ (@idOJ int,@idI int)
@@ -14,9 +14,9 @@ exec sp_Premesti_OJ 101, 1
 
 
 
-2.Kreirati uskladi?tenu proceduru sp_novo_OJ koja u tabelu Organizaciona jedinica dodaje novu jedinicu ?ija adresa i
-grad se zadaju i u nju zaposliti novog izvr?ioca ?ije ime i prezime se zadaju Postaviti datum zaposlenja za dana?nji
-datum kori?�enjem funkcije getDate.
+2.Kreirati uskladitenu proceduru sp_novo_OJ koja u tabelu Organizaciona jedinica dodaje novu jedinicu ?ija adresa i
+grad se zadaju i u nju zaposliti novog izvrioca čije ime i prezime se zadaju Postaviti datum zaposlenja za dananji
+datum korišenjem funkcije getDate.
 create procedure sp_novo_OJ (@adresa nchar(30),@grad nchar(30), @telefon nchar(30), @ime nchar(30), @prezime nchar(30)
 ,@email nchar(30),  @telefonIzv nchar(30), @jmbg nchar(13), @adresaIzv nchar(30), @gradIzv nchar(30), @cenaPoSatu float)
 as
@@ -31,9 +31,9 @@ exec sp_novo_OJ 'Kosmajska', 'Beograd', '066252147','Marija', 'Milicevic', 'mejl
 
 
 
-3.Kreirati uskladi?tenu proceduru sp_Ukloni koja izvr?ioca ?ije se ime i prezime zadaju uklanja sa projekta jedino
-ako zadati izvr?ilac radi samo na jednom projektu. U slucaju uspesnog uklanjanja sa projekta vratiti poruku
-?UKLONJEN SA PROJEKTA? u protivnom vratiti poruku ?NIJE UKLONJEN SA PROJEKTA?.
+3.Kreirati uskladitenu proceduru sp_Ukloni koja izvrioca ?ije se ime i prezime zadaju uklanja sa projekta jedino
+ako zadati izvrilac radi samo na jednom projektu. U slucaju uspesnog uklanjanja sa projekta vratiti poruku
+UKLONJEN SA PROJEKTA u protivnom vratiti poruku NIJE UKLONJEN SA PROJEKTA.
 create procedure sp_Ukloni
 @ime nchar(30),
 @prezime nchar(30)
@@ -55,4 +55,30 @@ select 'NIJE UKLONJENSA PROJEKTA'
 end
 end
 -----------------------------------------------
-exec sp_Ukloni 'Bojan', 'Jovanovi?'
+exec sp_Ukloni 'Bojan', 'Jovanović'
+
+
+4.Kreirati uskladitenu proceduru sp_Promena_Cena koja menja cenu rada po satu izvrioca Zadaje se ime , prezime
+i procenat za koji se cena rada menja. Ukoliko je promenjena cena manja od prose?ne cene rada po satu izvriti
+promenu i vratiti poruku Cena rada po satu je promenjena. U slucaju da je nova vrednost veca od prose?ne cene
+rada po satu, ne izvravati promenu i vratiti poruku Cena rada po satu nije promenjena..
+alter procedure sp_Promena_Cena (@ime nvarchar(30), @prezime nvarchar(30), @procenat int)
+as
+begin
+declare @prosek float
+select @prosek =avg(cenaPoSatu) from izvrsilac
+declare @novaPlata real
+select @novaPlata=cenaPoSatu+cenaPoSatu*@procenat/100 from izvrsilac
+where ime=@ime and prezime=@prezime
+If @novaPlata <(select avg(cenaPoSatu) from izvrsilac)
+begin
+update izvrsilac
+set cenaPoSatu=cenaPoSatu+cenaPoSatu*@procenat/100 
+where ime=@ime and prezime=@prezime
+select 'Promenjena cena';
+end
+else
+select 'Nije promenjena cena';
+end
+-------------------
+exec sp_Promena_Cena 'Dalibor', 'Grgurović', 3
---
+++ SQLQuery6.sql
@@ -0,0 +1,63 @@
+﻿CREATE TABLE predmet_profesor_log (
+id INT IDENTITY(1,1) PRIMARY KEY,
+id_predmeta INT NOT NULL,
+stari_profesor_id INT NOT NULL,
+novi_profesor_id INT NOT NULL,
+datum_promene DATETIME NOT NULL);
+-- Kreiranje trigera trig_UpdatePredmetProfesor
+CREATE TRIGGER trig_UpdatePredmetProfesor
+ON predmet
+AFTER UPDATE
+AS
+BEGIN
+-- Provera da li se UPDATE koji je okinuo triger odnosi na id_profesora
+IF UPDATE(ID_PROFESORA)
+BEGIN
+INSERT INTO predmet_profesor_log (id_predmeta, stari_profesor_id, novi_profesor_id, datum_promene)
+SELECT deleted.ID_PREDMETA, deleted.ID_PROFESORA, inserted.ID_PROFESORA, GETDATE()
+FROM inserted JOIN deleted ON inserted.ID_PREDMETA = deleted.ID_PREDMETA;
+END END;Kreirati triger koji sprečava brisanje zapisa iz tabele Student. Korisniku ispisati poruku o zabrani brisanja zapisa.
+CREATE TRIGGER trgInsteadOfDeleteStudent
+ON Student
+INSTEAD OF DELETE
+AS
+BEGIN
+-- Poruka korisniku
+print ('Brisanje studenata nije dozvoljeno.')
+END;
+
+
+Kreirati uskladištenu proceduru sa primenom transakcije koja smanjuje budzet projekta za zadati procenat i tako dobijena sredstva
+dodaje drugom projektu.Procedura prihavat id projekta kome se smanjuju sredstva,id projekta kome se dodaju sredstva i procenat.
+Create procedure sp_PrebaciSredstva1234(@idProjekta1 int, @idProjekta2 int, @procenat float) as
+begin
+declare @pom float
+begin tran
+Select @pom=budzet-budzet*@procenat/100
+FROM projekat
+WHERE idProjekat=@idProjekta1;
+update Projekat
+SET budzet=@pom
+WHERE idProjekat=@idProjekta1;
+if @@ROWCOUNT =0
+begin
+rollback /* ponistenje transakcije */
+print('NEUSPEŠNO');
+end
+else
+begin
+update Projekat
+SET budzet+=@pom
+WHERE idProjekat=@idProjekta2;
+if @@ROWCOUNT =0
+begin
+rollback
+print('NEUSPEŠNO');
+end
+else
+begin
+commit /* potvrda transakcije */
+print('USPEŠNO');
+end
+end
+end
