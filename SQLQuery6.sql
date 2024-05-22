2b5b37420eb018e35e5710ce5ac3835efbd140ba
Mihajlo Eskic <eskicmihajlo55@gmail.com>
2024-05-22 22:12:10 +0200

    triggers

--- SQLQuery6.sql
+++ SQLQuery6.sql
@@ -61,3 +61,80 @@ print('USPEŠNO');
 end
 end
 end
+
+
+
+
+CREATE PROCEDURE sp_smanji_cene_i_dodeli
+    @NoviIzvrsilacID INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRANSACTION;
+
+    BEGIN TRY
+        DECLARE @UkupnoSmanjenje DECIMAL(18, 2);
+        DECLARE @BrojIzvrsioca INT;
+
+        -- Izračunavanje ukupnog smanjenja od 1% za sve izvršioce
+        SELECT @UkupnoSmanjenje = SUM(CenaRada * 0.01)
+        FROM Izvrsioci
+        WHERE IzvrsilacID <> @NoviIzvrsilacID;
+
+        -- Smanjujemo cenu rada za 1% za sve izvršioce osim novog
+        UPDATE Izvrsioci
+        SET CenaRada = CenaRada * 0.99
+        WHERE IzvrsilacID <> @NoviIzvrsilacID;
+
+        -- Dodajemo ukupno smanjenje cene rada novom izvršiocu
+        UPDATE Izvrsioci
+        SET CenaRada = CenaRada + @UkupnoSmanjenje
+        WHERE IzvrsilacID = @NoviIzvrsilacID;
+
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END;
+
+
+
+
+CREATE TRIGGER trig_IzmenaOcene
+ON Zapisnik
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO Zapisnik_history (id_studenta, id_ispita, bodovi, ocena, datum_izmene)
+    SELECT 
+        deleted.id_studenta,
+        deleted.id_ispita,
+        deleted.bodovi,
+        deleted.ocena,
+        GETDATE()
+    FROM 
+        deleted
+    WHERE 
+        deleted.ocena <> inserted.ocena;
+END;
+
+
+CREATE TRIGGER trig_Dodaj_OJ
+ON organizaciona_jedinica
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO OJ_LOG (grad, adresa, datumDodavanja)
+    SELECT 
+        inserted.grad,
+        inserted.adresa,
+        GETDATE()
+    FROM 
+        inserted;
+END;
